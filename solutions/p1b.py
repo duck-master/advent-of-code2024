23815166457a76bcf1f8c612a7d41f1bca0050e4
duck_master <duckeagle1280@gmail.com>
2024-12-02 00:06:37 -0500

    adding identical parse_nums functino from p1a.py to p1b.py

--- solutions/p1b.py
+++ solutions/p1b.py
@@ -2,5 +2,18 @@
 Solves https://adventofcode.com/2024/day/1#part2
 """
 
+def parse_nums(input_file):
+    """
+    Parses a list of textlines from the input file
+    """
+    l1 = []
+    l2 = []
+    with open(input_file, mode = "r", encoding = "utf-8") as f:
+        for l in f:
+            ls = l.split("   ")
+            l1.append(int(ls[0]))
+            l2.append(int(ls[1]))
+    return l1, l2
+
 if __name__ == "__main__":
     print("hello world")
