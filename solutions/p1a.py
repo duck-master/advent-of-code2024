05b3f2e7b556d9b4e77332e15b488a5656831f1e
duck_master <duckeagle1280@gmail.com>
2024-12-02 00:26:18 -0500

    move definition of example and test_data into p1_utils.py

--- solutions/p1a.py
+++ solutions/p1a.py
@@ -2,18 +2,7 @@
 Solves https://adventofcode.com/2024/day/1
 """
 
-def parse_nums(input_file):
-    """
-    Parses a list of textlines from the input file
-    """
-    l1 = []
-    l2 = []
-    with open(input_file, mode = "r", encoding = "utf-8") as f:
-        for l in f:
-            ls = l.split("   ")
-            l1.append(int(ls[0]))
-            l2.append(int(ls[1]))
-    return l1, l2
+from p1_utils import parse_nums, example, test_data
 
 def find_diffs(l1, l2):
     """
@@ -24,8 +13,5 @@ def find_diffs(l1, l2):
     return sum(abs(a - b) for a, b in zip(sorted_l1, sorted_l2))
 
 if __name__ == "__main__":
-    example = parse_nums("../data/example_p1.txt")
     assert find_diffs(*example) == 11
-
-    test_data = parse_nums("../data/test_p1.txt")
     print(find_diffs(*test_data))
--- solutions/p1b.py
+++ solutions/p1b.py
@@ -2,18 +2,7 @@
 Solves https://adventofcode.com/2024/day/1#part2
 """
 
-def parse_nums(input_file):
-    """
-    Parses a list of textlines from the input file
-    """
-    l1 = []
-    l2 = []
-    with open(input_file, mode = "r", encoding = "utf-8") as f:
-        for l in f:
-            ls = l.split("   ")
-            l1.append(int(ls[0]))
-            l2.append(int(ls[1]))
-    return l1, l2
+from p1_utils import parse_nums
 
 def appearance_count_dict(somelist):
     """
@@ -39,8 +28,5 @@ def similarity_score(l1, l2):
     return score
 
 if __name__ == "__main__":
-    example = parse_nums("../data/example_p1.txt")
     assert similarity_score(*example) == 31
-
-    testdata = parse_nums("../data/test_p1.txt")
     print(similarity_score(*testdata))
