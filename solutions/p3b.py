f083793f575b3a6e458f734c42b9fc8b078e5783
duck_master <duckeagle1280@gmail.com>
2024-12-10 01:33:58 -0500

    implement find_instructions in p3b.py

--- solutions/p3b.py
+++ solutions/p3b.py
@@ -5,12 +5,13 @@ Solves https://adventofcode.com/2024/day/3#part2
 import re
 from utils import EXAMPLES, TESTS
 
+INSTRUCTION_REGEX = re.compile(r"(do(?:n't)?)\(\)|mul\((\d{1,3}),(\d{1,3})\)")
+
 def find_instructions(program):
     """
     Finds do(), don't(), or mul(X, Y) instructions
     """
-    # TODO
-    raise NotImplementedError
+    return re.findall(INSTRUCTION_REGEX, program)
 
 
 def find_mul_sum(program):
@@ -22,4 +23,5 @@ def find_mul_sum(program):
 
 
 if __name__ == "__main__":
+    print(find_instructions(EXAMPLES[2]))
     assert find_mul_sum(EXAMPLES[2]) == 48
