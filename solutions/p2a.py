eb321f41dd2c849d86167b3024a4337a7f751ff9
duck_master <duckeagle1280@gmail.com>
2024-12-10 00:42:23 -0500

    add is_monotonic(report) to p2a.py; disable superfluous-parens checker

--- solutions/p2a.py
+++ solutions/p2a.py
@@ -1,8 +1,39 @@
 """
 Solves https://adventofcode.com/2024/day/2
+
+Functions:
+* is_monotonic(report)
+* is_report_safe(report)
+* count_safe_reports(reports)
 """
 
 from utils import EXAMPLES, TESTS
+
+def is_monotonic(report):
+    """
+    Determines if the report is monotonic.
+
+    Args:
+    * report (list[int]): The report.
+
+    Returns:
+    * bool: Whether the report is monotonic
+    """
+    # edge case
+    if report[1] == report[0]:
+        return False
+
+    # check whether it should increase
+    should_increase = (report[1] > report[0])
+
+    # main loop
+    for next_val, prev_val in zip(report[1:], report[:-1]):
+        if next_val == prev_val:
+            return False
+        if (next_val > prev_val) != should_increase:
+            return False
+
+    return True
 
 def is_report_safe(report):
     """
@@ -33,4 +64,9 @@ def count_safe_reports(reports):
 
 
 if __name__ == "__main__":
+    # debug logic
+    for example_report in EXAMPLES[1]:
+        print(is_monotonic(example_report))
+
+    # main logic
     assert count_safe_reports(EXAMPLES[1]) == 2
