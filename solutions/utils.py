e7e9a9b891bcd16bee780351d464ecb47d53bac3
duck_master <duckeagle1280@gmail.com>
2024-12-10 00:05:44 -0500

    refactor solutions/utils.py

--- solutions/utils.py
+++ solutions/utils.py
@@ -1,19 +1,50 @@
 """
 Defines the utility function parse_nums
+
+Functions:
+* parse_p1(input_lines)
+* parse_file_group(parsers, file_paths)
+
+Data:
+* PARSERS
 """
 
-def parse_nums(input_file):
+def parse_p1(input_lines):
     """
-    Parses a list of textlines from the input file
+    Parses a list of lines
     """
     l1 = []
     l2 = []
-    with open(input_file, mode = "r", encoding = "utf-8") as f:
-        for l in f:
-            ls = l.split("   ")
-            l1.append(int(ls[0]))
-            l2.append(int(ls[1]))
+    for l in input_lines:
+        ls = l.split("   ")
+        l1.append(int(ls[0]))
+        l2.append(int(ls[1]))
     return l1, l2
 
-example = parse_nums("../data/example_p1.txt")
-test_data = parse_nums("../data/test_p1.txt")
+def parse_file_group(parsers, file_paths):
+    """
+    Parses a group of files at once.
+
+    Args:
+    * parsers (list[callable]): A list of functions.
+    * file_paths (list[str]): A list of file paths. 
+    """
+    result = []
+    # main loop
+    for parser, filepath in zip(parsers, file_paths):
+        with open(filepath, mode = "r", encoding = "utf-8") as f:
+            file_content = f.readlines()
+        parsed_content = parser(file_content)
+        result.append(parsed_content)
+    return result
+
+
+
+# expand as needed
+PARSERS = [
+    parse_p1
+]
+
+#TODO: refactor
+#example = parse_nums("../data/example_p1.txt")
+#test_data = parse_nums("../data/test_p1.txt")
