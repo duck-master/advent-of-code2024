706f6ac248f0bab2c3133cacaa98ed8f3c8f6758
duck_master <duckeagle1280@gmail.com>
2024-12-10 00:59:37 -0500

    add is_report_now_safe in p2b.py

--- solutions/p2b.py
+++ solutions/p2b.py
@@ -7,13 +7,25 @@ Functions:
 """
 
 from utils import EXAMPLES
+from p2a import is_report_safe
 
 def is_report_now_safe(report):
     """
     Determines whether the report is safe with at most one bad level
     """
-    # TODO
-    raise NotImplementedError
+    # try the original report
+    if is_report_safe(report):
+        return True
+
+    # if unsafe, try removing every level one at a time
+    for i, _ in enumerate(report):
+        new_report = report[:i] + report[i + 1:]
+        if is_report_safe(new_report):
+            return True
+
+    # if that doesn't work, we know it's false
+    return False
+
 
 def count_new_safe_reports(reports):
     """"
@@ -23,4 +35,8 @@ def count_new_safe_reports(reports):
     raise NotImplementedError
 
 if __name__ == "__main__":
+    # debug logic
+    for example_report in EXAMPLES[1]:
+        print(is_report_now_safe(example_report))
+
     assert count_new_safe_reports(EXAMPLES[1]) == 4
