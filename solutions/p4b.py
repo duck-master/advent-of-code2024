91186c1783ae46468678e8ab44fe0a9fea591c77
duck_master <duckeagle1280@gmail.com>
2024-12-16 22:15:20 -0500

    fill in read_cross in p4b.py

--- solutions/p4b.py
+++ solutions/p4b.py
@@ -5,13 +5,27 @@ Solves https://adventofcode.com/2024/day/4#part2
 from utils import EXAMPLES
 from p4a import find_chars
 
+CROSS_DIRECTIONS = [
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1)
+    ]
+
 def read_cross(grid, center):
     """
-    Reads the points at a cross
+    Reads the grid cells in a cross centered at the given center
     """
-    #TODO
-    raise NotImplementedError
-
+    n = len(grid)
+    x_c, y_c = center
+    result = []
+    for x_d, y_d in CROSS_DIRECTIONS:
+        x_n = x_c + x_d
+        y_n = y_c + y_d
+        if x_n not in range(n) or y_n not in range(n):
+            return None
+        result.append(grid[x_n][y_n])
+    return result
 
 def find_X_MASes(grid):
     """
