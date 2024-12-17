f19b4b1c14a4a90376c7f41c9e73bcf0b3c01fd0
duck_master <duckeagle1280@gmail.com>
2024-12-16 20:31:09 -0500

    add DIRECTIONS variable, refactor find_Xes, fill in extend_path in p4a.py

--- solutions/p4a.py
+++ solutions/p4a.py
@@ -5,6 +5,18 @@ Solves https://adventofcode.com/2024/day/4
 from pprint import pprint
 from utils import EXAMPLES
 
+#hardcode all the valid directions
+DIRECTIONS = {
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+}
+
 def find_Xes(grid):
     """
     finds all X's
@@ -13,15 +25,19 @@ def find_Xes(grid):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == "X":
-                result.append([(x, y)])
+                result.append((x, y))
     return result
 
 def extend_path(start, direction, length):
     """
     extends a path in the corresponding direction
     """
-    #TODO
-    raise NotImplementedError
+    x_s, y_s = start
+    x_d, y_d = direction
+    result = [(x_s + i*x_d, y_s + i*y_d)
+              for i in range(length)
+              ]
+    return result
 
 def read_path(grid, path):
     """
@@ -40,6 +56,7 @@ def find_XMAS_count(grid):
 if __name__ == "__main__":
     # debugging logic
     stage1 = find_Xes(EXAMPLES[4])
+    pprint(stage1)
 
     # main test
     pprint(find_XMAS_count(EXAMPLES[4]))
