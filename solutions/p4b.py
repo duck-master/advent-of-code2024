2dd2b54c36914a720b23abbd0db7f44da454b39d
duck_master <duckeagle1280@gmail.com>
2024-12-16 21:43:30 -0500

    adding code outline for p4b.py

---
+++ solutions/p4b.py
@@ -0,0 +1,39 @@
+"""
+Solves https://adventofcode.com/2024/day/4#part2
+"""
+
+from utils import EXAMPLES
+
+def find_As(grid):
+    """
+    Finds all A's within the grid
+    """
+    #TODO
+    raise NotImplementedError
+
+def read_cross(grid, center):
+    """
+    Reads the points at a cross
+    """
+    #TODO
+    raise NotImplementedError
+
+
+def find_X_MASes(grid):
+    """
+    finds all instances of two MASes in a cross
+    returns a list
+    """
+    #TODO
+    raise NotImplementedError
+
+
+def find_X_MAS_count(grid):
+    """
+    finds the total number of two MASes in a cross
+    """
+    #TODO
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    print(find_X_MAS_count(EXAMPLES[4]))
