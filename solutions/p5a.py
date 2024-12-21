04546a02e39fdaf3c0fae977304e179b2ede774e
duck_master <duckeagle1280@gmail.com>
2024-12-20 23:20:07 -0500

    fill in check_update_in_order in p5a.py

--- solutions/p5a.py
+++ solutions/p5a.py
@@ -6,11 +6,23 @@ from utils import EXAMPLES
 
 def check_update_in_order(update, ordering_rules):
     """
-    Checks if all the updates are in order
+    Checks if this particular update is in order
     """
-    # TODO
-    raise NotImplementedError
+    # main loop
+    for rule in ordering_rules:
+        first, second = rule
+        # skip the rule if irrelevant
+        if first not in update or second not in update:
+            continue
 
+        # otherwise, find both indices
+        first_index = update.index(first)
+        second_index = update.index(second)
+        if first_index >= second_index:
+            return False            # return immediately
+
+    # if all rules work, return True
+    return True
 
 def get_middle_number(update):
     """
@@ -29,4 +41,10 @@ def get_sum_of_correct_updates(input_data):
 
 
 if __name__ == "__main__":
+    # debug logic
+    test_rules = EXAMPLES[5][0]
+    test_updates = EXAMPLES[5][1]
+    test_check = [check_update_in_order(test_update, test_rules) for test_update in test_updates]
+    assert test_check == [True, True, True, False, False, False]
+    # main logic
     print(get_sum_of_correct_updates(EXAMPLES[5]))
