6a41c3cdae43e0185d4b09b58bde1b6f7a5cf13c
duck_master <duckeagle1280@gmail.com>
2024-12-20 22:21:47 -0500

    add code outline for p5a.py

---
+++ solutions/p5a.py
@@ -0,0 +1,32 @@
+"""
+Solves https://adventofcode.com/2024/day/5
+"""
+
+from utils import EXAMPLES
+
+def check_update_in_order(update, ordering_rules):
+    """
+    Checks if all the updates are in order
+    """
+    # TODO
+    raise NotImplementedError
+
+
+def get_middle_number(update):
+    """
+    Gets the middle number of the update
+    """
+    # TODO
+    raise NotImplementedError
+
+
+def get_sum_of_correct_updates(input_data):
+    """
+    Gets the sum of the correct updates
+    """
+    # TODO
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    print(get_sum_of_correct_updates(EXAMPLES[5]))
